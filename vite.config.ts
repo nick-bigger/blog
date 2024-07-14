edcf4a6876b1a5693bf3016e45f13142103aaf2b
nick.bigger <nick.bigger@gmail.com>
2024-07-14 11:35:57 -0500

    Add base to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import react from "@vitejs/plugin-react"
-import path from "path"
-import { defineConfig } from "vite"
- 
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
+
 export default defineConfig({
+  base: "/blog/",
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
